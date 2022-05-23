43646bfa0f70d92e8c003e06a4dc976c5322de07
thesh1ro <gabrielmonteiro1111@gmail.com>
2022-05-23 14:04:39 -0300

    ajustes

--- src/components/HeaderArea.vue
+++ src/components/HeaderArea.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <ul class="nav-about">
-      <img class="nav-item" src="" alt="Logo" />
+      <img class="nav-item nav-logo" src="@/assets/Logo.png" alt="Logo" />
       <li class="nav-item">Rifas.net</li>
     </ul>
     <ul class="nav-user">
